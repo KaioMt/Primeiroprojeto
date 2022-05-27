e568be3353c483b118d536ab968070767ead3ba2
KaioMt <morettokaio0@gmail.com>
2022-05-27 19:16:23 -0300

    Enviando outros arquivos

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-const http = require('http');
 
-const hostname = '127.0.0.1';//Endereço do servidor
-const port = 3000 //Porta do servidor
-
-const serve = http.createServer((req, res) => { //Criando o servidor
-    res.statusCode = 200 // Status ok de resposta
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Ola Mundo uba');
-});
-
-serve.listen(port, hostname, () => { 
-
-    console.log(`Servidor rodando em http://${hostname}:${port}/`)
+const express=require('express');// determinando agit utilização do express
+const app=express();
+const path=require ('path');//endereço de cada rota
+const router=express.Router();//trabalha com rotas
+router.get('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
+router.get('/about', function(req,res){
+    res.sendFile(path.join(__dirname+'/about.html'));
+})
+
+app.use('/',router);
+app.use('/about',router);
+app.listen(process.env.port||3000);
+console.log("Servidor rodando");
---
+++ tarefa.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';//Endereço do servidor
+const port = 3000 //Porta do servidor
+
+const serve = http.createServer((req, res) => { //Criando o servidor
+    res.statusCode = 200 // Status ok de resposta
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Ola Mundo');
+});
+
+serve.listen(port, hostname, () => { 
+
+    console.log(`Servidor rodando em http://${hostname}:${port}/`)
+})
